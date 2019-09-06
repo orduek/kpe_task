9600ee77cc8f10c8f9a7911c3eec4f4a567aa5c5
orduek <orduek@gmail.com>
2019-09-06 12:00:55 -0400

    Added NBS analysis to compare networks

--- connectivityAnalysis.py
+++ connectivityAnalysis.py
@@ -26,6 +26,7 @@ from nilearn import plotting
 # shen
 atlas_filename = '/home/or/Downloads/shenPar/shen_1mm_268_parcellation.nii.gz'
 atlas_labes = pd.read_csv('/home/or/Downloads/shenPar/shen_268_parcellation_networklabels.csv')
+colors = pd.read_csv('/home/or/Downloads/shenPar/shen_268_parcellation_networklabels_colors.csv')
 coords = plotting.find_parcellation_cut_coords(labels_img=atlas_filename)
 
 atlas_labes = np.array(atlas_labes)
@@ -117,7 +118,10 @@ os.chdir('/home/or/kpe_conn/ShenParc')
 np.save("session_1Timeseries_Shen",session1) # saving array
 np.save("session_2TimeseriesShen",session2)
 np.save("session_3TimeseriesShen", session3)
-#session1 = list(np.load('session_2Timeseries.npy', allow_pickle = True)) # loading the saved array
+#%% load sessions
+session1 = list(np.load('session_1Timeseries_Shen.npy', allow_pickle = True)) # loading the saved array
+sessoin2 = list(np.load('session_2TimeseriesShen.npy', allow_pickle = True)) # loading the saved array
+session3 = list(np.load('session_3TimeseriesShen.npy', allow_pickle = True))
 #%% Build and save each subject's timeseries in a file
 def saveSubjectSeries(subject_list, session, timeSerArray):
     # First we create a new directory for the session
@@ -248,18 +252,38 @@ def pos_neg (corr_thr):
     return positive_corr , negative_corr
 
 
-#%% do Trauma first script
+#%% do Trauma first script - 1st session
 trauma_1st_ses = creatSubCor(subject_list, '1', 'speficTrial_0_trauma.npy')
 trauma_1st_thr = tresholMat(trauma_1st_ses, 0.01)
 pos_corr , neg_corr = pos_neg(trauma_1st_thr)
 np.savetxt("pos_cor_Traum1.csv" , pos_corr, delimiter= ",")
 np.savetxt("neg_cor_Traum1.csv" , neg_corr, delimiter= ",")
+
+#% second session
+trauma_2_1st_ses = creatSubCor(subject_list, '1', 'speficTrial_4_trauma.npy')
+trauma_2_1st_ses_thr = tresholMat(trauma_2_1st_ses, 0.01)
+pos_corr , neg_corr = pos_neg(trauma_2_1st_ses_thr)
+np.savetxt("pos_cor_Traum2_ses1.csv" , pos_corr, delimiter= ",")
+np.savetxt("neg_cor_Traum2_ses1.csv" , neg_corr, delimiter= ",")
 #%% do trauma on second session
 trauma_2nd_ses = creatSubCor(subject_list, '2', 'speficTrial_0_trauma.npy')
 trauma_2ndSes_corr = tresholMat(trauma_2nd_ses, 0.01)
 pos_corr , neg_corr = pos_neg(trauma_2ndSes_corr)
 np.savetxt("pos_cor_TraumSes2.csv" , pos_corr, delimiter= ",")
 np.savetxt("neg_cor_TraumSes2.csv" , neg_corr, delimiter= ",")
+
+# check difference between first and second script:
+deltaMatrix = np.mean(np.array(trauma_2_1st_ses), axis = 0) - np.mean(np.array(trauma_1st_ses), axis = 0) # contrast the two matrices
+# run -ttest
+tr_1_2Ttest = scipy.stats.ttest_rel(trauma_1st_ses, trauma_2_1st_ses, axis = 0)
+p_vals = tr_1_2Ttest[1]
+delta_mat_thr = np.array(deltaMatrix)
+# now I can treshold the mean matrix
+fwe_thr = 0.05/(len(p_vals)*(len(p_vals)-1)/2)
+delta_mat_thr[p_vals>fwe_thr] = 0 # everything that has p value larger than treshold becomes zero 
+numNonZero = np.count_nonzero(delta_mat_thr)
+print (f'Number of edges crossed the FWE thr is {numNonZero}')
+
 #%% Do sad first
 sad_corr_subs = creatSubCor(subject_list, '1', 'speficTrial_2_sad.npy') # take sad
 sad_corr_thr = tresholMat(sad_corr_subs, 0.01)
@@ -276,10 +300,10 @@ plotting.plot_matrix(sad_corr_thr, labels=atlas_labes[:,0], colorbar=True, tri =
 plotting.plot_matrix(correlation_Trauma_0_Mean_thr, labels=atlas_labes[:,0], colorbar=True, tri = 'diag', grid = 'color')
 
 # plot in connectome
-plotting.plot_connectome(correlation_Trauma_0_Mean_thr, coords, edge_threshold='95%', colorbar=True, black_bg = True, annotate = True)
+plotting.plot_connectome(trauma_1st_thr, coords, edge_threshold='95%', colorbar=True, black_bg = True, annotate = True)
 
 ##
-plotting.plot_matrix(correlation_positive, labels=atlas_labes[:,0], colorbar=True, tri = 'diag', grid = 'color')
+plotting.plot_matrix(trauma_2_1st_ses_thr, labels=atlas_labes[:,0], colorbar=True, tri = 'diag', grid = 'color')
 #%% Graph theory calculations (Degree centrality etc)
 import networkx as nx
 G = nx.from_numpy_array(trauma_0_corr)
@@ -296,5 +320,42 @@ sorted(l,key = l.get, reverse = True)
 plotting.plot_connectome(correlation_matrix, coords,
                              edge_threshold=0.7, colorbar=True, black_bg = True, annotate = True)
 #%% Compare matrices between sessions
-    
+deltaMatrix = trauma_2ndSes_corr - trauma_1st_thr # contrast the two matrices
+# run -ttest
+tr_1_2Ttest = scipy.stats.ttest_rel(trauma_1st_ses, trauma_2nd_ses, axis = 0)
+p_vals = tr_1_2Ttest[1]
+delta_mat_thr = np.array(deltaMatrix)
+# now I can treshold the mean matrix
+fwe_thr = 0.05/(len(p_vals)*(len(p_vals)-1)/2)
+delta_mat_thr[tr_1_2Ttest[1]>fwe_thr] = 0 # everything that has p value larger than treshold becomes zero 
+numNonZero = np.count_nonzero(delta_mat_thr)
+print (f'Number of edges crossed the FWE thr is {numNonZero}')
+# maybe we should run network based statistics to compare the two networks
 
+#%% plotting stuff
+# take colors
+colors = pd.read_csv('/home/or/Downloads/shenPar/shen_268_parcellation_networklabels_colors.csv')
+color_node = list(colors["color"])
+# plot in specific region as seed
+# create 268 x 268 array
+empty = np.zeros((268,268))
+# take only left amygdala connections
+empty[227,:] = trauma_1st_thr[227,:]
+empty[:,227] = trauma_1st_thr[:,227]
+# plot
+plotting.plot_connectome(empty, coords, edge_threshold='95%', colorbar=True, black_bg = True, annotate = True, node_color = color_node)
+
+# plot in browser
+view = plotting.view_connectome(empty, coords, threshold='90%') 
+view.open_in_browser() 
+view_color.open_in_browser()
+
+
+#%% Run Network Based Analysis
+# first reshape the matrix dimensions (right now its [subs,x,y]) to [x,y,subs]
+trt1Reshape = np.moveaxis(np.array(trauma_1st_ses),0,-1)
+trt2Reshape = np.moveaxis(np.array(trauma_2_1st_ses),0,-1)
+
+from bct import nbs
+# we compare ket1 and ket3
+pval, adj, _ = nbs.nbs_bct(trt1Reshape, trt2Reshape, thresh=2.5, tail='both',k=500, paired=True, verbose = True)
---
+++ vbm.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 30 14:52:49 2019
+
+@author: Or Duek
+VBM analysis - based on https://nilearn.github.io/auto_examples/02_decoding/plot_oasis_vbm.html
+
+"""
+
+#%% 
+
+
+
+#%%
